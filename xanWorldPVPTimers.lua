dc4d2cff33224bd177a83fbf02511d8c2ce7c44e
Xruptor <xruptor@manaflux.com>
2012-04-18 11:17:26 -0400

    -Updated RestoreLayout and SaveLayout to be more uniform with the rest of my addons.

--- xanWorldPVPTimers.lua
+++ xanWorldPVPTimers.lua
@@ -510,62 +510,49 @@ end
 --------------------------]]
 
 function f:SaveLayout(frame)
-
+	if type(frame) ~= "string" then return end
+	if not _G[frame] then return end
 	if not XanWPT_DB then XanWPT_DB = {} end
+	
+	local opt = XanWPT_DB[frame] or nil
 
-	local opt = XanWPT_DB[frame] or nil;
-
-	if opt == nil then
+	if not opt then
 		XanWPT_DB[frame] = {
 			["point"] = "CENTER",
 			["relativePoint"] = "CENTER",
-			["PosX"] = 0,
-			["PosY"] = 0,
+			["xOfs"] = 0,
+			["yOfs"] = 0,
 		}
-		opt = XanWPT_DB[frame];
+		opt = XanWPT_DB[frame]
+		return
 	end
 
-	local f = getglobal(frame);
-	local scale = f:GetEffectiveScale();
-	opt.PosX = f:GetLeft() * scale;
-	opt.PosY = f:GetTop() * scale;
-	
+	local point, relativeTo, relativePoint, xOfs, yOfs = _G[frame]:GetPoint()
+	opt.point = point
+	opt.relativePoint = relativePoint
+	opt.xOfs = xOfs
+	opt.yOfs = yOfs
 end
 
 function f:RestoreLayout(frame)
+	if type(frame) ~= "string" then return end
+	if not _G[frame] then return end
+	if not XanWPT_DB then XanWPT_DB = {} end
 
-	if not XanWPT_DB then XanWPT_DB = {} end	
+	local opt = XanWPT_DB[frame] or nil
 
-	local f = getglobal(frame);
-	local opt = XanWPT_DB[frame] or nil;
-
-	if opt == nil then
+	if not opt then
 		XanWPT_DB[frame] = {
 			["point"] = "CENTER",
 			["relativePoint"] = "CENTER",
-			["PosX"] = 0,
-			["PosY"] = 0,
+			["xOfs"] = 0,
+			["yOfs"] = 0,
 		}
-		opt = XanWPT_DB[frame];
+		opt = XanWPT_DB[frame]
 	end
 
-	local x = opt.PosX;
-	local y = opt.PosY;
-	local s = f:GetEffectiveScale();
-
-	if (not x or not y) or (x==0 and y==0) then
-		f:ClearAllPoints();
-		f:SetPoint("CENTER", UIParent, "CENTER", 0, 0);
-		return 
-	end
-
-	--calculate the scale
-	x,y = x/s,y/s;
-
-	--set the location
-	f:ClearAllPoints();
-	f:SetPoint("TOPLEFT", UIParent, "BOTTOMLEFT", x, y);
-
+	_G[frame]:ClearAllPoints()
+	_G[frame]:SetPoint(opt.point, UIParent, opt.relativePoint, opt.xOfs, opt.yOfs)
 end
 
 if IsLoggedIn() then f:PLAYER_LOGIN() else f:RegisterEvent("PLAYER_LOGIN") end
